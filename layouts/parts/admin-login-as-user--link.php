608d9288d37f86e133761fd2ca3bced5bd407dd2
Welington De Lima Olegario <wolegario49@gmail.com>
2024-10-17 15:59:05 -0300

    ajusta a lógica e identificar se o ID recebido é de um agente ou de um usuário

--- layouts/parts/admin-login-as-user--link.php
+++ layouts/parts/admin-login-as-user--link.php
@@ -7,4 +7,6 @@
 use MapasCulturais\i;
 
 ?>
-<mc-link v-if="global.auth.is('admin')" route="auth/asUserId" :params="{id: entity.id}" icon="arrow-up" class="button button--primary button--sm button--icon" right-icon><?= i::__('logar')?></mc-link>
+
+<mc-link v-if="global.auth.is('admin') && !entity.userId" route="auth/asUserId" :params="{id:entity.id}" icon="arrow-up" class="button button--primary button--sm button--icon" right-icon><?= i::__('logar')?></mc-link>
+<mc-link v-if="global.auth.is('admin') && entity.userId" route="auth/asUserId" :params="{id:entity.userId, agent_id: entity.id}" icon="arrow-up" class="button button--icon button--sm login" right-icon><?= i::__('logar')?></mc-link>
